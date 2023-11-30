976d91f97651d263fd19fa86767af2a9d36400f1
raddevus <1radeutsch1@gmail.com>
2023-11-30 14:21:22 -0500

    Initial Commit

---
+++ ThreadStudy/ThreadStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadStudyApp.swift
+//  ThreadStudy
+//
+//  Created by roger deutsch on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

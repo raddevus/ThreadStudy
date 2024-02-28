480d13d186eb48c87056cb53ae4c69b36919f6a9
roger deutsch <rogerdeutsch@radMacPro.local>
2024-02-28 15:40:49 -0500

    Figured out how to handle Urlsession.shared.data call to api so that I handle exceptions thrown, bad responses (status codes) and also handle decoding JSON in it's own exception wrapper.  This helps to know which error is occurring (data retrieval or JSON decode).

--- ThreadStudy/Worker.swift
+++ ThreadStudy/Worker.swift
@@ -29,7 +29,7 @@ struct Worker{
     func getChuckNorrisData() -> Task<String, Error>{
         Task{
             let (data, response) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
-            print("### response \(response) ####")
+            // print("### response \(response) ####")
             let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
             print (decodedResponse?.value ?? " yada yo!")
             return decodedResponse?.value ?? "FAILED!"
@@ -37,19 +37,91 @@ struct Worker{
     }
     
     func getChuckNorrisData2() async -> String{
+        var data: Data? = nil
         do {
-            let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
-            let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
-            print (decodedResponse?.value ?? " yada yo!")
-            return decodedResponse?.value ?? "FAILED!"
+            let (data, retError) = try await (URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random-")!) as? (Data, HTTPURLResponse))!
+            print("printing data : \(data)")
+            print ("response error --->  \(retError.statusCode)")
+            // catches errors in response from web api -- so web api has been
+            // successfully contacted but a "normal" status error occurs
+            // Other bad errors throw exceptions which is caught below
+            // bad URL like string:"https://api.chucknorris.io/jokes/random-"
+            // will cause this error (404)
+            switch retError.statusCode{
+                case _ where retError.statusCode < 200 || retError.statusCode >= 300:
+                    return "Couldn't carry on.  Bad status code from API \(retError.statusCode)"
+                default:
+                    print("Success!")
+            }
         }
         catch{
-            print (error)
+            // catches errors when the URL is not well formed
+            // bad URL like the following causes this exception:
+            // (notice the x in the protocol portion of URL
+            // string:"httpxs://api.chucknorris.io/jokes/random"
+            print("!! What ERROR? \(error)")
+            return "failed to retrieve data"
         }
+        do {
+            let decodedResponse = try JSONDecoder().decode(Fake.self, from: data!)
+            //print (decodedResponse?.value ?? " chuck 2")
+            return decodedResponse.x ?? "FAILED!"
+        }
+        catch{
+            print ("!!!!   error! occurred: \(error)  !!!!")
+        }
+        
         return "failed!"
+    }
+    
+    func getInitialPokemonList() async -> String{
+        var data: Data? = nil
+        do{
+            let (data, response) = try await ((URLSession.shared.data(from: URL(string:"")!) as? (Data, HTTPURLResponse))!)
+            switch response.statusCode{
+                case _ where response.statusCode < 200 || response.statusCode >= 300:
+                    return "Couldn't carry on.  Bad status code from API \(response.statusCode)"
+                default:
+                    print("Success!")
+            }
+        }
+        catch{
+            // catches errors when the URL is not well formed
+            // bad URL like the following causes this exception:
+            // (notice the x in the protocol portion of URL
+            // string:"httpxs://api.chucknorris.io/jokes/random"
+            print("Failed \(error)")
+            return "BAD URL - Failed to retrieve data."
+        }
+        
+        do{
+            let response = try JSONDecoder().decode(PokemonChar.self, from: data!)
+        }
+        catch {
+            print("*** error: Couldn't DECODE JSON \(error) ***")
+        }
+        return "completed method"
+    }
+    
+    struct Fake: Codable{
+        // just so I can force and decode error
+        let x: String
+        let value: String
     }
     
     struct Joke: Codable{
         let value: String
+    }
+    
+    struct PokemonBase{
+        let count: Int
+        let next: String
+        let previous: String
+        let results: [PokemonChar]
+    }
+    
+    struct PokemonChar : Decodable{
+        let name: String
+        let url: String
     }
 }
